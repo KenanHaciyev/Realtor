6f8c0aa3d40cf09c28b77fc9470cfad6c7c96272
Kenan <kenan.haciyev.123@bk.ru>
2024-02-24 23:21:27 +0400

    Add routes and pages

---
+++ src/pages/index.ts
@@ -0,0 +1,8 @@
+import Home from "./Home";
+import Profile from "./Profile";
+import Offers from "./Offers";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+import ForgotPassword from "./ForgotPassword";
+
+export { Home, Profile, Offers, SignIn, SignUp, ForgotPassword };
